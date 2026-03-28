2b0050fca73be03e00ff48dd6122bffd04d17925
audiosistem <audio2sistem@yahoo.com>
2026-03-28 18:06:26 +0200

    Up Round

--- matrix/service.subtitles.wyzie/resources/lib/key.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# Fișierul key.py
-api_keys = [
-    "AIzaSyC-U0ZsN3yMFgXUqrEu72N_3iAQZO2IkyU",
-    "AIzaSyCWW8OXnc7QwIUTs_W0FCEVrZEm3qliDzk",
-    "AIzaSyCEYe59xoog4g8GvqPOrBOP-veGVY8IFqI",
-]
