7402872eaf4d1e868e5de3aad3f456c8e887ea0e
audiosistem <audio2sistem@yahoo.com>
2026-04-17 18:04:00 +0300

    Up Round

---
+++ matrix/script.module.viperscrapers/lib/viperscrapers/sources_viperscrapers/torrents/bitmagnet.py
@@ -0,0 +1,170 @@
+# created by kodifitzwell for Fenomscrapers
+"""
+	Fenomscrapers Project
+"""
+
+import xml.etree.ElementTree as ET
+import queue
+from viperscrapers.modules import client
+from viperscrapers.modules import source_utils
+
+
+class source:
+	timeout = 10
+	priority = 1
+	pack_capable = True
+	hasMovies = True
+	hasEpisodes = True
+	_queue = queue.SimpleQueue()
+	def __init__(self):
+		self.language = ['en']
+		self.base_link = "https://bitmagnetfortheweebs.midnightignite.me"
+		self.movieSearch_link = '/torznab/api?t=movie&imdbid=%s'
+		self.tvSearch_link = '/torznab/api?t=tvsearch&imdbid=%s&season=%s&ep=%s'
+		self.min_seeders = 0
+
+	def sources(self, data, hostDict):
+		sources = []
+		if not data: return sources
+		sources_append = sources.append
+		try:
+			title = data['tvshowtitle'] if 'tvshowtitle' in data else data['title']
+			title = title.replace('&', 'and').replace('Special Victims Unit', 'SVU').replace('/', ' ')
+			aliases = data['aliases']
+			episode_title = data['title'] if 'tvshowtitle' in data else None
+			year = data['year']
+			imdb = data['imdb']
+			if 'tvshowtitle' in data:
+				season = data['season']
+				episode = data['episode']
+				hdlr = 'S%02dE%02d' % (int(season), int(episode))
+				url = '%s%s' % (self.base_link, self.tvSearch_link % (imdb, season, episode))
+			else:
+				hdlr = year
+				url = '%s%s' % (self.base_link, self.movieSearch_link % imdb)
+			# log_utils.log('url = %s' % url)
+			try:
+				results = client.request(url, timeout=self.timeout)
+				files = ET.fromstring(results)
+			except:
+				files = ET.fromstring('<?xml version="1.0" ?><metadata />')
+				raise
+			finally:
+				self._queue.put_nowait(files) # if seasons
+				self._queue.put_nowait(files) # if shows
+			undesirables = source_utils.get_undesirables()
+			check_foreign_audio = source_utils.check_foreign_audio()
+		except:
+			source_utils.scraper_error('BITMAGNET')
+			return sources
+
+		for file in files.iter('item'):
+			try:
+				attr_dict = {'title': file.find('title').text}
+				for attr in file.findall('torznab:attr', {'torznab': 'http://torznab.com/schemas/2015/feed'}):
+					key, val = attr.get('name'), attr.get('value')
+					if key: attr_dict[key] = val
+				hash = attr_dict['infohash']
+
+				name = source_utils.clean_name(attr_dict['title'])
+
+				if not source_utils.check_title(title, aliases, name.replace('.(Archie.Bunker', ''), hdlr, year): continue
+				name_info = source_utils.info_from_name(name, title, year, hdlr, episode_title)
+				if source_utils.remove_lang(name_info, check_foreign_audio): continue
+				if undesirables and source_utils.remove_undesirables(name_info, undesirables): continue
+
+				url = 'magnet:?xt=urn:btih:%s&dn=%s' % (hash, name)
+
+				try:
+					seeders = int(attr_dict['seeders'])
+					if self.min_seeders > seeders: continue
+				except: seeders = 0
+
+				quality, info = source_utils.get_release_quality(name_info, url)
+				try:
+					size = float(attr_dict['size'])
+					dsize, isize = source_utils.convert_size(size)
+					info.insert(0, isize)
+				except: dsize = 0
+				info = ' | '.join(info)
+
+				sources_append({
+					'source': 'torrent', 'language': 'en', 'direct': False, 'debridonly': True,
+					'provider': 'bitmagnet', 'hash': hash, 'url': url, 'name': name, 'name_info': name_info,
+					'quality': quality, 'info': info, 'size': dsize, 'seeders': seeders
+				})
+			except:
+				source_utils.scraper_error('BITMAGNET')
+		return sources
+
+	def sources_packs(self, data, hostDict, search_series=False, total_seasons=None, bypass_filter=False):
+		sources = []
+		if not data: return sources
+		sources_append = sources.append
+		try:
+			title = data['tvshowtitle'].replace('&', 'and').replace('Special Victims Unit', 'SVU').replace('/', ' ')
+			aliases = data['aliases']
+			imdb = data['imdb']
+			year = data['year']
+			season = data['season']
+			url = '%s%s' % (self.base_link, self.tvSearch_link % (imdb, season, data['episode']))
+			files = self._queue.get(timeout=self.timeout + 1)
+			undesirables = source_utils.get_undesirables()
+			check_foreign_audio = source_utils.check_foreign_audio()
+		except:
+			source_utils.scraper_error('BITMAGNET')
+			return sources
+
+		for file in files.iter('item'):
+			try:
+				attr_dict = {'title': file.find('title').text}
+				for attr in file.findall('torznab:attr', {'torznab': 'http://torznab.com/schemas/2015/feed'}):
+					key, val = attr.get('name'), attr.get('value')
+					if key: attr_dict[key] = val
+				hash = attr_dict['infohash']
+
+				name = source_utils.clean_name(attr_dict['title'])
+
+				episode_start, episode_end = 0, 0
+				if not search_series:
+					if not bypass_filter:
+						valid, episode_start, episode_end = source_utils.filter_season_pack(title, aliases, year, season, name.replace('.(Archie.Bunker', ''))
+						if not valid: continue
+					package = 'season'
+
+				elif search_series:
+					if not bypass_filter:
+						valid, last_season = source_utils.filter_show_pack(title, aliases, imdb, year, season, name.replace('.(Archie.Bunker', ''), total_seasons)
+						if not valid: continue
+					else: last_season = total_seasons
+					package = 'show'
+
+				name_info = source_utils.info_from_name(name, title, year, season=season, pack=package)
+				if source_utils.remove_lang(name_info, check_foreign_audio): continue
+				if undesirables and source_utils.remove_undesirables(name_info, undesirables): continue
+
+				url = 'magnet:?xt=urn:btih:%s&dn=%s' % (hash, name)
+				try:
+					seeders = int(attr_dict['seeders'])
+					if self.min_seeders > seeders: continue
+				except: seeders = 0
+
+				quality, info = source_utils.get_release_quality(name_info, url)
+				try:
+					size = float(attr_dict['size'])
+					dsize, isize = source_utils.convert_size(size)
+					info.insert(0, isize)
+				except: dsize = 0
+				info = ' | '.join(info)
+
+				item = {
+					'source': 'torrent', 'language': 'en', 'direct': False, 'debridonly': True,
+					'provider': 'bitmagnet', 'hash': hash, 'url': url, 'name': name, 'name_info': name_info,
+					'quality': quality, 'info': info, 'size': dsize, 'seeders': seeders, 'package': package
+				}
+				if search_series: item.update({'last_season': last_season})
+				elif episode_start: item.update({'episode_start': episode_start, 'episode_end': episode_end}) # for partial season packs
+				sources_append(item)
+			except:
+				source_utils.scraper_error('BITMAGNET')
+		return sources
